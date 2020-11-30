5eecec14aa8b73c61ec2527db631d380a4271f1a
Adam Copp <adam.copp@gmail.com>
2020-11-30 13:24:36 +0000

    WIP: Add test to check that a broken archive can actually be repaired.

--- pkg/par2/par2_test.go
+++ pkg/par2/par2_test.go
@@ -55,3 +55,24 @@ func TestRepairValidArchive(t *testing.T) {
 		t.Fatalf("Intact archive threw error when asked to repair: %v", err)
 	}
 }
+
+func TestRepairBrokenFiles(t *testing.T) {
+	f, err := os.Open("testdata/sample.broken.mp4.par2")
+	defer f.Close()
+	if err != nil {
+		t.Fatalf("Could not open encoded par2 file: %v", err)
+	}
+
+	archive, err := FromFiles("testdata", f)
+	if err != nil {
+		t.Fatalf("Could not create Archive from file: %v", err)
+	}
+
+	if err = archive.Repair(); err != nil {
+		t.Fatalf("Intact archive threw error when asked to repair: %v", err)
+	}
+
+	// TODO: Test that the files were actually fixed
+	//   Will require unpacking to a new directory instead of fixing in-place.
+	//   Use go's testing package temporary directory, and setup/teardown to reset.
+}
