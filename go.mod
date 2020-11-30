e62980afdbcfd31abbe64a0c1ab9609292a57835
Adam Copp <adam.copp@gmail.com>
2020-11-30 16:26:25 +0000

    Introduce failing test to check archive verification

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/esteth/usenet
 go 1.12
 
 require (
-	github.com/pkg/errors v0.8.1
-	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
-	golang.org/x/tools/gopls v0.4.0 // indirect
+	github.com/pkg/errors v0.9.1
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
+	golang.org/x/tools/gopls v0.5.3 // indirect
 )
--- pkg/par2/par2_test.go
+++ pkg/par2/par2_test.go
@@ -1,9 +1,85 @@
 package par2
 
 import (
+	"bytes"
+	"io"
 	"os"
+	"path"
 	"testing"
 )
+
+func copyFile(t *testing.T, src string, dst string) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		t.Fatalf("Could not stat copy src %s: %v", src, err)
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		t.Fatalf("%s is not a regular file", src)
+	}
+
+	destFileStat, err := os.Stat(dst)
+	if err != nil {
+		t.Fatalf("Could not stat copy dst %s: %v", dst, err)
+	}
+
+	if destFileStat.IsDir() {
+		dst = path.Join(dst, path.Base(src))
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		t.Fatalf("Could not open copy src %s: %v", src, err)
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		t.Fatalf("Could not create copy dst %s: %v", dst, err)
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		t.Fatalf("Could not copy src %s to dst %s: %v", src, dst, err)
+	}
+}
+
+func areFileContentsEqual(t *testing.T, file1 string, file2 string) bool {
+	f1, err := os.Open(file1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f1.Close()
+
+	f2, err := os.Open(file2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f2.Close()
+
+	for {
+		b1 := make([]byte, 1024)
+		_, err1 := f1.Read(b1)
+
+		b2 := make([]byte, 1024)
+		_, err2 := f2.Read(b2)
+
+		if err1 != nil || err2 != nil {
+			if err1 == io.EOF && err2 == io.EOF {
+				return true
+			} else if err1 == io.EOF || err2 == io.EOF {
+				return false
+			} else {
+				t.Fatal(err1, err2)
+			}
+		}
+
+		if !bytes.Equal(b1, b2) {
+			return false
+		}
+	}
+}
 
 func TestValidateValidArchive(t *testing.T) {
 	f, err := os.Open("testdata/sample.mp4.par2")
@@ -57,7 +133,14 @@ func TestRepairValidArchive(t *testing.T) {
 }
 
 func TestRepairBrokenFiles(t *testing.T) {
-	f, err := os.Open("testdata/sample.broken.mp4.par2")
+	tempDir := t.TempDir()
+	copyFile(t, "testdata/sample.broken.mp4", tempDir)
+	copyFile(t, "testdata/sample.broken.mp4.par2", tempDir)
+	copyFile(t, "testdata/sample.broken.mp4.vol0+1.PAR2", tempDir)
+	copyFile(t, "testdata/sample.broken.mp4.vol1+2.PAR2", tempDir)
+	copyFile(t, "testdata/sample.broken.mp4.vol3+2.PAR2", tempDir)
+
+	f, err := os.Open(path.Join(tempDir, "sample.broken.mp4.par2"))
 	defer f.Close()
 	if err != nil {
 		t.Fatalf("Could not open encoded par2 file: %v", err)
@@ -72,7 +155,7 @@ func TestRepairBrokenFiles(t *testing.T) {
 		t.Fatalf("Intact archive threw error when asked to repair: %v", err)
 	}
 
-	// TODO: Test that the files were actually fixed
-	//   Will require unpacking to a new directory instead of fixing in-place.
-	//   Use go's testing package temporary directory, and setup/teardown to reset.
+	if !areFileContentsEqual(t, path.Join(tempDir, "sample.broken.mp4"), "testdata/sample.mp4") {
+		t.Errorf("Repaired file contents not same as reference file")
+	}
 }
