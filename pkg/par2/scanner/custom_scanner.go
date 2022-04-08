f9c278bb1fe842e7f9dc6be69ccaba11bfac5ea9
Adam Copp <adam.copp@gmail.com>
2022-04-08 23:05:04 +0100

    Implement scanning for all base packet types

--- pkg/par2/scanner/custom_scanner.go
+++ pkg/par2/scanner/custom_scanner.go
@@ -5,16 +5,24 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"strings"
 )
 
+const HEADER_LENGTH = 64
+
 type CustomScanner struct {
-	reader io.Reader
+	source ScannerSource
 	packet Packet
 	err    error
 }
 
+type ScannerSource interface {
+	io.Reader
+	io.Seeker
+}
+
 type packetHeader struct {
-	length        uint64
+	packetLength  uint64
 	md5Hash       [16]byte
 	recoverySetId [16]byte
 	packetType    [16]byte
@@ -22,7 +30,7 @@ type packetHeader struct {
 
 func (c *CustomScanner) Next() {
 	magicSequenceBytes := make([]byte, 8)
-	if _, err := io.ReadFull(c.reader, magicSequenceBytes); err != nil {
+	if _, err := io.ReadFull(c.source, magicSequenceBytes); err != nil {
 		c.err = fmt.Errorf("could not peek for magic sequence: %w", err)
 		return
 	}
@@ -31,22 +39,22 @@ func (c *CustomScanner) Next() {
 		return
 	}
 
-	header, err := readHeader(c.reader)
+	header, err := readHeader(c.source)
 	if err != nil {
 		c.err = fmt.Errorf("could not read packet header: %w", err)
 	}
 
 	packetTypeString := string(header.packetType[:])
 	if packetTypeString == mainPacketType {
-		c.packet, c.err = scanMainPacket(c.reader)
+		c.packet, c.err = scanMainPacket(c.source, header)
 	} else if packetTypeString == fileDescriptionPacketType {
-		c.packet, c.err = scanFileDescriptionPacket(c.reader)
+		c.packet, c.err = scanFileDescriptionPacket(c.source)
 	} else if packetTypeString == fileSliceChecksumPacketType {
-		c.packet, c.err = scanFileSliceChecksumPacket(c.reader)
+		c.packet, c.err = scanFileSliceChecksumPacket(c.source, header)
 	} else if packetTypeString == recoverySlicePacketType {
-		c.packet, c.err = scanRecoverySlicePacket(c.reader)
+		c.packet, c.err = scanRecoverySlicePacket(c.source, filePath)
 	} else if packetTypeString == creatorPacketType {
-		c.packet, c.err = scanCreatorPacket(c.reader)
+		c.packet, c.err = scanCreatorPacket(c.source, header)
 	} else {
 		c.packet = &unknownPacket{
 			typ: packetTypeString,
@@ -54,16 +62,16 @@ func (c *CustomScanner) Next() {
 	}
 }
 
-func scanMainPacket(reader io.Reader) (MainPacket, error) {
+func scanMainPacket(source ScannerSource, header packetHeader) (MainPacket, error) {
 	var packet MainPacket
 
-	sliceSize, err := readUint64(reader)
+	sliceSize, err := readUint64(source)
 	if err != nil {
 		err = fmt.Errorf("could not read slice size from main packet: %w", err)
 		return packet, err
 	}
 
-	numRecoveryFiles, err := readUint32(reader)
+	numRecoveryFiles, err := readUint32(source)
 	if err != nil {
 		err = fmt.Errorf("could not read number of recovery files from main packet: %w", err)
 		return packet, err
@@ -71,16 +79,18 @@ func scanMainPacket(reader io.Reader) (MainPacket, error) {
 
 	recoveryFileIDs := make([][16]byte, numRecoveryFiles)
 	for i := uint32(0); i < numRecoveryFiles; i++ {
-		if _, err = io.ReadFull(reader, recoveryFileIDs[i][:]); err != nil {
-			return packet, fmt.Errorf("could not read md5 hash from packet header: %w", err)
+		if _, err = io.ReadFull(source, recoveryFileIDs[i][:]); err != nil {
+			return packet, fmt.Errorf("could not read recovery file ID %i from main packet: %w", i, err)
 		}
 	}
 
-	numNonRecoveryFiles := (binary.LittleEndian.Uint64(data[8:16]) - 28 + 16*uint64(numRecoveryFiles)) / 16
-	nonRecoveryFilesBaseIndex := uint64(12 + 16*numRecoveryFiles)
+	// The number of non-recovery file IDs is the remaining space in the packet
+	numNonRecoveryFiles := (header.packetLength - HEADER_LENGTH - 12 - 16*uint64(numRecoveryFiles)) / 16
 	nonRecoveryFileIDs := make([][16]byte, numNonRecoveryFiles)
 	for i := uint64(0); i < numNonRecoveryFiles; i++ {
-		copy(nonRecoveryFileIDs[i][:], mainPacketData[nonRecoveryFilesBaseIndex+16*i:nonRecoveryFilesBaseIndex+16+16*i])
+		if _, err = io.ReadFull(source, nonRecoveryFileIDs[i][:]); err != nil {
+			return packet, fmt.Errorf("could not read non recovery file ID %i from main packet: %w", i, err)
+		}
 	}
 
 	return MainPacket{
@@ -90,40 +100,88 @@ func scanMainPacket(reader io.Reader) (MainPacket, error) {
 	}, nil
 }
 
-func scanFileDescriptionPacket(reader io.Reader) (packet FileDescriptionPacket, err error) {
+func scanFileDescriptionPacket(source ScannerSource) (packet FileDescriptionPacket, err error) {
+	if _, err = io.ReadFull(source, packet.ID[:]); err != nil {
+		return packet, fmt.Errorf("could not read file ID from file description packet: %w", err)
+	}
+	if _, err = io.ReadFull(source, packet.MD5[:]); err != nil {
+		return packet, fmt.Errorf("could not read MD5 hash from file description packet: %w", err)
+	}
+	if _, err = io.ReadFull(source, packet.MD516[:]); err != nil {
+		return packet, fmt.Errorf("could not read MD5-16 from file description packet: %w", err)
+	}
+	packet.Length, err = readUint64(source)
+	if err != nil {
+		err = fmt.Errorf("could not read file length from file description packet: %w", err)
+		return packet, err
+	}
 
+	return
 }
 
-func scanFileSliceChecksumPacket(reader io.Reader) (packet FileSliceChecksumPacket, err error) {
+func scanFileSliceChecksumPacket(source ScannerSource, header packetHeader) (packet FileSliceChecksumPacket, err error) {
+	if _, err = io.ReadFull(source, packet.FileID[:]); err != nil {
+		return packet, fmt.Errorf("could not read file ID from file slice checksum packet: %w", err)
+	}
+	numSlices := (header.packetLength - 16 - HEADER_LENGTH) / 20
+	packet.SliceHashes = make([][16]byte, numSlices)
+	packet.SliceCRC32s = make([][4]byte, numSlices)
 
+	for i := uint64(0); i < numSlices; i++ {
+		if _, err = io.ReadFull(source, packet.SliceHashes[i][:]); err != nil {
+			return packet, fmt.Errorf("could not read hash %i from file slice checksum packet: %w", i, err)
+		}
+		if _, err = io.ReadFull(source, packet.SliceCRC32s[i][:]); err != nil {
+			return packet, fmt.Errorf("could not read CRC32 %i from file slice checksum packet: %w", i, err)
+		}
+	}
+
+	return
 }
 
-func scanRecoverySlicePacket(reader io.Reader) (packet RecoverySlicePacket, err error) {
-
-}
-
-func scanCreatorPacket(reader io.Reader) (packet CreatorPacket, err error) {
-
-}
-
-func readHeader(reader io.Reader) (header packetHeader, err error) {
-	packetLengthBytes := make([]byte, 8)
-	if _, err = io.ReadFull(reader, packetLengthBytes); err != nil {
+func scanRecoverySlicePacket(source ScannerSource, filePath string) (packet RecoverySlicePacket, err error) {
+	if packet.Exponent, err = readUint32(source); err != nil {
+		err = fmt.Errorf("could not read exponent from recovery slice packet", err)
 		return
 	}
-	header.length = binary.LittleEndian.Uint64(packetLengthBytes)
+	if packet.Data.FileOffset, err = source.Seek(0, io.SeekCurrent); err != nil {
+		err = fmt.Errorf("could not read current file position while parsing recovery slice packet", err)
+		return
+	}
+	packet.Data.FilePath = filePath
 
-	if _, err = io.ReadFull(reader, header.md5Hash[:]); err != nil {
+	return
+}
+
+func scanCreatorPacket(source ScannerSource, header packetHeader) (packet CreatorPacket, err error) {
+	identifierBytes := make([]byte, header.packetLength-HEADER_LENGTH)
+	if _, err = io.ReadFull(source, identifierBytes); err != nil {
+		err = fmt.Errorf("could not read creator packet identifier: %w", err)
+		return
+	}
+
+	packet.Creator = strings.TrimRight(string(identifierBytes[:]), "\000")
+	return
+}
+
+func readHeader(source ScannerSource) (header packetHeader, err error) {
+	packetLengthBytes := make([]byte, 8)
+	if _, err = io.ReadFull(source, packetLengthBytes); err != nil {
+		return
+	}
+	header.packetLength = binary.LittleEndian.Uint64(packetLengthBytes)
+
+	if _, err = io.ReadFull(source, header.md5Hash[:]); err != nil {
 		err = fmt.Errorf("could not read md5 hash from packet header: %w", err)
 		return
 	}
 
-	if _, err = io.ReadFull(reader, header.recoverySetId[:]); err != nil {
+	if _, err = io.ReadFull(source, header.recoverySetId[:]); err != nil {
 		err = fmt.Errorf("could not read recover set ID from packet header: %w", err)
 		return
 	}
 
-	if _, err = io.ReadFull(reader, header.packetType[:]); err != nil {
+	if _, err = io.ReadFull(source, header.packetType[:]); err != nil {
 		err = fmt.Errorf("could not read packet type from packet header: %w", err)
 		return
 	}
@@ -131,9 +189,9 @@ func readHeader(reader io.Reader) (header packetHeader, err error) {
 	return
 }
 
-func readUint32(reader io.Reader) (result uint32, err error) {
+func readUint32(source ScannerSource) (result uint32, err error) {
 	bytes := make([]byte, 4)
-	if _, err = io.ReadFull(reader, bytes); err != nil {
+	if _, err = io.ReadFull(source, bytes); err != nil {
 		err = fmt.Errorf("could not little endian number: %w", err)
 		return
 	}
@@ -141,9 +199,9 @@ func readUint32(reader io.Reader) (result uint32, err error) {
 	return
 }
 
-func readUint64(reader io.Reader) (result uint64, err error) {
+func readUint64(source ScannerSource) (result uint64, err error) {
 	bytes := make([]byte, 8)
-	if _, err = io.ReadFull(reader, bytes); err != nil {
+	if _, err = io.ReadFull(source, bytes); err != nil {
 		err = fmt.Errorf("could not little endian number: %w", err)
 		return
 	}
